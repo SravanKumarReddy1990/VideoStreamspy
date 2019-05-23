c6c38532b96081f754b22c972ad5224622b5b1c8
sravan <bskreddy1990@gmail.com>
2019-05-24 00:35:33 +0530

    first commit

--- camera.py
+++ camera.py
@@ -6,7 +6,17 @@ class Camera(object):
     files 1.jpg, 2.jpg and 3.jpg at a rate of one frame per second."""
 
     def __init__(self):
-        self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
+        #self.frames = [open(f + '.jpg', 'rb').read() for f in ['1', '2', '3']]
 
     def get_frame(self):
-        return self.frames[int(time()) % 3]
+        vidcap = cv2.VideoCapture('VID-20190112-WA0003.mp4')
+        success,image = vidcap.read()
+        count = 0
+        success = True
+        while success:
+            self.frames(image)
+            #cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
+            success,image = vidcap.read()
+            print 'Read a new frame: ', success
+            count += 1
+        return self.frames[int(time()) % count]
