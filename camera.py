41cebe98d8a54813bbdf7348fbe4817be6871064
sravan <bskreddy1990@gmail.com>
2019-05-24 12:52:49 +0530

    first commit

--- camera.py
+++ camera.py
@@ -12,13 +12,5 @@ class Camera(object):
     def get_frame(self):
         vidcap = cv2.VideoCapture('VID-20190112-WA0003.mp4')
         success,image = vidcap.read()
-        count = 0
-        success = True
-        while success:
-            self.frames[image]
-            #cv2.imwrite("frame%d.jpg" % count, image)     
-            # save frame as JPEG file
-            success,image = vidcap.read()
-            print('Read a new frame: ', success)
-            count += 1
-        return self.frames[int(time()) % count]
+        ret, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
